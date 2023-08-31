4d8c90e9e7ec040292bebfce6398517506cc1d60
kjunda01 <kjunda01@sinvaljuniorlms@gmail.com>
2023-08-30 22:09:42 -0300

    025 - Exercício Python #016 - Quebrando um número

--- Curso em Video/Exercicios/025 - Exercício Python #016 - Quebrando um número.py
+++ Curso em Video/Exercicios/025 - Exercício Python #016 - Quebrando um número.py
@@ -4,11 +4,13 @@ Crie um programa que leia um número real qualquer pelo teclado e mostre na tela
 
 '''
 while True:
+    
     try:
         numero = float(input('Digite um número: '))
         parte_inteira = str(numero).split('.')[0]
         parte_decimal = str(numero).split('.')[1]
         break
+
     except ValueError:
         print('Digite apenas numeros reais. Ex. (1,2), (-36.45), (12)...')
 
