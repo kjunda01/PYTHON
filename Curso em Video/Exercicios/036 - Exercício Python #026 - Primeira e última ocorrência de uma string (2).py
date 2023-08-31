583bf3a8c70a0afc8a46f79b41585a12af95e5ab
Kjunda01 <sinvaljuniorlms@gmail.com>
2023-08-31 10:03:35 -0300

    036 - Exercício Python #026 - Primeira e última ocorrência de uma string (2)

--- Curso em Video/Exercicios/036 - Exercício Python #026 - Primeira e última ocorrência de uma string (2).py
+++ Curso em Video/Exercicios/036 - Exercício Python #026 - Primeira e última ocorrência de uma string (2).py
@@ -12,3 +12,4 @@ ulti_a = frase.rfind("A") + 1
 print(f'A letra A aparece ({quant_a}) vezes na frase.')
 print(f'A primeira letra A apareceu na posição ({pri_a})')
 print(f'A última letra A apareceu na posição ({ulti_a})')
+
