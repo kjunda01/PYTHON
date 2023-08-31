d8a91c83d5243f1a317ef02e7acaaee27fa13537
kjunda01 <kjunda01@sinvaljuniorlms@gmail.com>
2023-08-30 22:08:47 -0300

    017 - Exercício Python #009 - Tabuada

--- Curso em Video/Exercicios/017 - Exercício Python #009 - Tabuada.py
+++ Curso em Video/Exercicios/017 - Exercício Python #009 - Tabuada.py
@@ -5,11 +5,13 @@ Desafio 09 - Faça um programa que leia um número inteiro qualquer e mostre na 
 '''
 
 while True:
+    
     try:
         numero = int(input('Digite o número para a tabuada: '))
         print(f'Tabuada do número: ({numero})')
         for tabuada in range(1,11):
             print(f'({tabuada}) x ({numero}) = ({tabuada * numero})')
             continue
+
     except ValueError:
         print('Digite apenas números inteiros.\n')
