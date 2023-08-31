5133bbd14bebf00af4fe705de8623f4b67c9c145
Kjunda01 <sinvaljuniorlms@gmail.com>
2023-08-31 10:03:18 -0300

    035 - Exercício Python #025 - Procurando uma string dentro de outra

--- Curso em Video/Exercicios/035 - Exercício Python #025 - Procurando uma string dentro de outra.py
+++ Curso em Video/Exercicios/035 - Exercício Python #025 - Procurando uma string dentro de outra.py
@@ -8,3 +8,4 @@ nome = input("Digite seu nome completo: ").upper()
 primeiro_nome = nome.split(" ")[0].upper()
 print(f"Vamos conferir.")
 print(f'{primeiro_nome}, seu nome possui SILVA.') if "SILVA" in nome else print("Não possui SILVA no nome.")
+
