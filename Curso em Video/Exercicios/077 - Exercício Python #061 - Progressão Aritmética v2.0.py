6b5a272ba5c79be9ab748dc212ec731c72a92d0b
kjunda01 <sinvaljuniorlms@gmail.com>
2024-04-20 11:07:29 -0300

    commit

---
+++ Curso em Video/Exercicios/077 - Exercício Python #061 - Progressão Aritmética v2.0.py
@@ -0,0 +1,13 @@
+print("Gerador de PA")
+print("-=" * 10)
+termo = int(input("Primeiro termo: "))
+razao = int(input("Razão: "))
+
+an = a + (n-1) d
+
+
+
+for n in range(termo, ultimo):
+    print(n)
+    ultimo = contador
+    
